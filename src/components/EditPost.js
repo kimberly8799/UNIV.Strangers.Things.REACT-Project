87c241c2bf746e5c3a9f9ffbedc8354ca44b7385
Kimberly Phan <kimberly.t.phan01@sjsu.edu>
2023-05-02 18:59:57 -0700

    uncomment stuff...

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import { getAPI } from '../api'
 
-const EditPost = ({ post }) => {
+const EditPost = ({ }) => {
 
-    const edit = async (event) => {
-        const response = await getAPI({
-            path: `/posts/${post._id}`,
-            method: "PATCH",
-            token,
-            body: {
-                post: {
-                    title,
-                    description,
-                    price,
-                    willDeliver,
-                    location
-                }
-            }
-        });
+
     }
     return (
         <>
            
         </>
     )
-}
 
 export default EditPost;
--- src/components/PostBox.js
+++ src/components/PostBox.js
@@ -1,58 +1,58 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 
-const PostBox = ({}) => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    const [location, setLocation] = useState('');
-    const [willDeliver, setWillDeliver] = useState(false);
+// const PostBox = ({}) => {
+//     const [title, setTitle] = useState('');
+//     const [description, setDescription] = useState('');
+//     const [price, setPrice] = useState('');
+//     const [location, setLocation] = useState('');
+//     const [willDeliver, setWillDeliver] = useState(false);
 
-    return (
-        <>
-        <div className="createPostBox">
-               <label htmlFor="title">Title</label>
-               <input
-                   type="text"
-                   onChange={event => setTitle(event.target.value)}
-                   value={title}
-               />
-               <br></br>
-               <label htmlFor="location">Location</label>
-               <input
-                   type="text"
-                   onChange={event => setLocation(event.target.value)}
-                   value={location}
-               />
-               <br></br>
-               <label htmlFor="description">Description</label>
-               <input
-                   id="inputBoxContainer"
-                   type="text"
-                   onChange={event => setDescription(event.target.value)}
-                   value={description}
-               />
-               <br></br>
-               <label htmlFor="price">Price</label>
-               <input
-                   type="text"
-                   onChange={event => setPrice(event.target.value)}
-                   value={price}
-               />
-               <div>
-               <br></br>
-               <label htmlFor="willDeliver">Will Deliver{" "}</label>
-               <input
-                   type="checkbox"
-                   onClick={() => setWillDeliver(!willDeliver)}
-                   value={willDeliver}
-               />
-               </div>
-               <br></br>
-               <button type="submit">create</button>
-           </div>
-        </>
-    )
-}
+//     return (
+//         <>
+//         <div className="createPostBox">
+//                <label htmlFor="title">Title</label>
+//                <input
+//                    type="text"
+//                    onChange={event => setTitle(event.target.value)}
+//                    value={title}
+//                />
+//                <br></br>
+//                <label htmlFor="location">Location</label>
+//                <input
+//                    type="text"
+//                    onChange={event => setLocation(event.target.value)}
+//                    value={location}
+//                />
+//                <br></br>
+//                <label htmlFor="description">Description</label>
+//                <input
+//                    id="inputBoxContainer"
+//                    type="text"
+//                    onChange={event => setDescription(event.target.value)}
+//                    value={description}
+//                />
+//                <br></br>
+//                <label htmlFor="price">Price</label>
+//                <input
+//                    type="text"
+//                    onChange={event => setPrice(event.target.value)}
+//                    value={price}
+//                />
+//                <div>
+//                <br></br>
+//                <label htmlFor="willDeliver">Will Deliver{" "}</label>
+//                <input
+//                    type="checkbox"
+//                    onClick={() => setWillDeliver(!willDeliver)}
+//                    value={willDeliver}
+//                />
+//                </div>
+//                <br></br>
+//                <button type="submit">create</button>
+//            </div>
+//         </>
+//     )
+// }
 
-export default PostBox;
+// export default PostBox;
